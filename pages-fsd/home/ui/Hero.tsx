03d93b84b82b80662127b384fd48871693406281
Alexandre Aittouares <alex.aittouares@gmail.com>
2026-06-06 03:26:54 +0200

    Implement Hero section with carousel on the home page

--- pages-fsd/home/ui/Hero.tsx
+++ pages-fsd/home/ui/Hero.tsx
@@ -1,4 +1,31 @@
+import { Button } from '@/shared/ui/shadcn/button'
+import Link from 'next/link'
+import HeroCarousel from './HeroCarousel'
+
 function Hero() {
-  return <div>Hero</div>
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque et
+          voluptas saepe in quae voluptate, laborum maiores possimus illum
+          reprehenderit aut delectus veniam cum perferendis unde sint doloremque
+          non nam.
+        </p>
+        <Button
+          render={<Link href="/products" />}
+          nativeButton={false}
+          size="lg"
+          className="mt-10"
+        >
+          Our Products
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  )
 }
 export default Hero
