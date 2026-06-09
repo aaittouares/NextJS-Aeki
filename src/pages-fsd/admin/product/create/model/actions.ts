366cfb50195d140619349a60193f1a5e61e627fc
Alexandre Aittouares <alex.aittouares@gmail.com>
2026-06-09 20:46:58 +0200

    Add zod product schema and validation to createProductAction

--- src/pages-fsd/admin/product/create/model/actions.ts
+++ src/pages-fsd/admin/product/create/model/actions.ts
@@ -3,6 +3,7 @@
 import { FormResponse } from '@/shared/types/action-function'
 import { getAuthUser, renderError } from './helpers'
 import prisma from '@/shared/lib/prisma/prisma.provider'
+import { productSchema } from '@/entities/product/model/product.schema'
 
 export const createProductAction = async (
   prevState: FormResponse,
@@ -11,21 +12,13 @@ export const createProductAction = async (
   const user = await getAuthUser()
 
   try {
-    const name = formData.get('name') as string
-    const company = formData.get('company') as string
-    const price = Number(formData.get('price') as string)
-    const image = formData.get('image') as File
-    const description = formData.get('description') as string
-    const featured = Boolean(formData.get('featured') as string)
+    const rawData = Object.fromEntries(formData)
+    const validatedFields = productSchema.parse(rawData)
 
     await prisma.product.create({
       data: {
-        name,
-        company,
-        price,
-        image: '/images/hero1.jpg',
-        description,
-        featured,
+        ...validatedFields,
+        image: '/images/hero2.jpg',
         clerkId: user.id,
       },
     })
